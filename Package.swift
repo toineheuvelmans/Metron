b93c74787e9b319289fb2b11b66b432c3c2bea7e
Ian Bytchek <ianbytchek@gmail.com>
2019-02-05 13:21:34 +0000

    Update Swift package description to latest 4.2 format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-    name: "Metron"
+    name: "Metron",
+    products: [.library(name: "Metron", targets: ["Metron"])],
+    targets: [.target(name: "Metron", dependencies: [], path: "./Metron")]
 )
